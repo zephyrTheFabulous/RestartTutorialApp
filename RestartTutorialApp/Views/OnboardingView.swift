6fc76dc565a332f83506ffec250d2e75d87429dd
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-08-23 01:53:21 +0700

    Add footer section with customized slider button

--- RestartTutorialApp/Views/OnboardingView.swift
+++ RestartTutorialApp/Views/OnboardingView.swift
@@ -45,6 +45,50 @@ struct OnboardingView: View {
         }
         Spacer()
           //MARK: - FOOTER
+        ZStack {
+          // 1. Background (Static)
+          Capsule()
+            .fill(.white.opacity(0.2))
+          Capsule()
+            .fill(.white.opacity(0.2))
+            .padding(8)
+
+          // 2. Call-to-action (Static)
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundStyle(.white)
+            .offset(x: 20)
+
+          // 3. Capsule (Dynamic width)
+          HStack {
+            Capsule()
+              .fill(.red)
+              .frame(width: 80)
+            Spacer()
+          }
+
+          // 4. Circle button (Draggable)
+          HStack {
+            ZStack {
+              Circle()
+                .fill(.colorRed)
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            }
+            .foregroundStyle(.white)
+            .frame(width: 80, height: 80, alignment: .center)
+            .onTapGesture {
+              isOnboardingViewActive = false
+            }
+            Spacer()
+          }
+        } //: FOOTER
+        .frame(height: 80, alignment: .center)
+        .padding()
 
 //        Text("Onboarding")
 //          .font(.largeTitle)
