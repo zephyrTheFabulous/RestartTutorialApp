f52f2624afcc556c3b4e03f7e8837bf21e33855b
zephyrTheFabulous <zephyrthefabulous@gmail.com>
2025-08-22 22:41:07 +0700

    Initial Commit

---
+++ RestartTutorialApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RestartTutorialApp
+//
+//  Created by Anthony on 22/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RestartTutorialApp/RestartTutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartTutorialAppApp.swift
+//  RestartTutorialApp
+//
+//  Created by Anthony on 22/8/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartTutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
